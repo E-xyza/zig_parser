5a9c2b90fe9a2f8bd1e0553923bfe9e588be6b55
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2022-10-14 14:07:55 -0500

    first commit

---
+++ lib/zig_parser.ex
@@ -0,0 +1,18 @@
+defmodule ZigParser do
+  @moduledoc """
+  Documentation for `ZigParser`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ZigParser.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/zig_parser_test.exs
@@ -0,0 +1,8 @@
+defmodule ZigParserTest do
+  use ExUnit.Case
+  doctest ZigParser
+
+  test "greets the world" do
+    assert ZigParser.hello() == :world
+  end
+end
